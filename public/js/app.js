2c183c1e9eca9293ee37b4e96afaec9549cbee7d
lesliecc106 <you@example.com>
2025-12-20 22:23:34 +0800

    Remove old loadDashboardAnalytics function calls

--- public/js/app.js
+++ public/js/app.js
@@ -390,10 +390,8 @@ function loadPageData(pageId) {
     switch(pageId) {
         case 'homePage':
         case 'dashboardPage':
-            loadDashboardAnalytics();
             loadStats();
             break;
-            loadDashboardAnalytics();
             loadStats();
             break;
         case 'historyPage':
